0d6b052d8e039a1dd08edadcdbde408fc09a0174
20201pf.cc0050 <20201pf.cc0050@pc-225992.passofundo.ifsul.local>
2022-04-22 11:19:29 -0300

    Commit inicial.

---
+++ src/ExtendsThreads1/ExtendsThreads1.java
@@ -0,0 +1,10 @@
+
+package ExtendsThreads1;
+
+/**
+ *
+ * @author 20201pf.cc0050
+ */
+public class ExtendsThreads1 {
+   
+}
---
+++ src/ExtendsThreads1/Main.java
@@ -0,0 +1,14 @@
+
+package ExtendsThreads1;
+
+/**
+ *
+ * @author 20201pf.cc0050
+ */
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+    }   
+}
