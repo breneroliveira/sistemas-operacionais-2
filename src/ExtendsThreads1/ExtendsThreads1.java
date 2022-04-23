575cebe5e842cc7047c007edbded80ec095d0ea7
Brener Augusto de Oliveira <breneroliveirad@gmail.com>
2022-04-23 12:46:42 -0300

    Questão 2 finalizada.

--- src/ExtendsThreads1/ExtendsThreads1.java
+++ src/ExtendsThreads1/ExtendsThreads1.java
@@ -1,10 +1,41 @@
 
 package ExtendsThreads1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author 20201pf.cc0050
  */
-public class ExtendsThreads1 {
-   
+public class ExtendsThreads1 extends Thread {
+    public ExtendsThreads1(String textoOperacao) {
+        super(textoOperacao);
+    }
+    
+    public void run() {
+        try {
+            Scanner entrada = new Scanner(System.in); // Cria uma instância da classe Scanner.
+            
+            //System.out.println("Informe a operação: ");
+            //String texto = entrada.nextLine(); // Lê uma string e atribui a variável "texto".
+            
+            System.out.println("Informe o primeiro número: ");
+            String numero1 = entrada.nextLine();
+            
+            System.out.println("Informe o segundo número: ");
+            String numero2 = entrada.nextLine();
+            
+            double numeroConvertido1 = Double.parseDouble(numero1);
+            double numeroConvertido2 = Double.parseDouble(numero2);
+            double resultadoOperacao;
+            
+            if(this.getName() == "S") {
+                resultadoOperacao = numeroConvertido1 + numeroConvertido2;
+                System.out.println("Resultado: " + resultadoOperacao);
+            } //else if()
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }   
 }
--- src/ExtendsThreads1/Main.java
+++ src/ExtendsThreads1/Main.java
@@ -9,6 +9,20 @@ package ExtendsThreads1;
 public class Main {
     
     public static void main(String[] args) {
-        // Teste.
+        // Soma.
+        ExtendsThreads1 thread1 = new ExtendsThreads1("S");
+        thread1.start();
+        
+        // Subtração.
+        /*ExtendsThreads1 thread2 = new ExtendsThreads1("Sub");
+        thread1.start();
+        
+        // Muultiplicação.
+        ExtendsThreads1 thread3 = new ExtendsThreads1("M");
+        thread1.start();
+        
+        // Divisão.
+        ExtendsThreads1 thread4 = new ExtendsThreads1("D");
+        thread1.start();*/
     }   
 }
